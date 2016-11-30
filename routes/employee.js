af11de11dfa554356eb6b0fbe47d9e2a74f58cee
pratik <lazy.monk.13@gmail.com>
2016-11-29 23:16:51 -0800

    find by id

--- routes/employee.js
+++ routes/employee.js
@@ -11,6 +11,11 @@ router.route('/')
 				done();
 				return res.status(500).send(err);
 			}
+			client.query(`SELECT * FROM employee`, (err, result) => {
+				if(err) res.status(500).send(err);
+				res.send(result.rows);
+				client.end(err => {if(err) throw err});
+			})
 		})
 	})
 	.post((req, res) => {
@@ -21,13 +26,31 @@ router.route('/')
 				return res.status(500).send(err);
 			}
 
-			const query =	client.query(`INSERT INTO
+			client.query(`INSERT INTO
 					employee (first_name, last_name, employee_id, is_admin)
 					VALUES ('${first_name}', '${last_name}', ${employee_id}, ${is_admin})`);
 
-			query.on('end', () => {
+			done();
+			res.send();
+		})
+	})
+
+router.route('/:employee_id')
+	.get((req, res) => {
+		const { employee_id } = req.params;
+		pg.connect(connectionString, (err, client, done) => {
+			if(err) {
 				done();
-				res.send("done")
+				return res.status(500).send(err);
+			}
+			client.query(`SELECT * FROM employee WHERE employee_id = ${employee_id}`, (err, result) => {
+				if(err) res.status(500).send(err);
+				if(result.rows.length) {
+					res.send(result.rows);
+				} else {
+					res.send('Employee not found');
+				}
+				client.end(err => {if(err) throw err});
 			})
 		})
 	})
